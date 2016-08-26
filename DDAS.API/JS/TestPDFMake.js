f1c59f2a20b98192a667f3ed2e453554fb60e0d5
Pradeep Chavhan <pradeep.chavhan@claritytechnologies.com>
2016-08-26 18:18:22 +0530

    Committing after updating references in the Libraries folder

---
+++ DDAS.API/JS/TestPDFMake.js
@@ -0,0 +1,20 @@
+﻿Function()
+{
+    var doc = new jsPDF();
+
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    doc.fromHTML($('#render_me').get(0), 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+
+    $('a').click(function () {
+        alert("downloading...");
+        doc.save('TestHTMLDoc.pdf');
+    });
+}
