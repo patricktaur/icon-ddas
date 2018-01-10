47577502b1d99e9762f8d2575f544bc4249c79cf
johnly.sam <johnly.sam@claritytechnologies.com>
2018-01-10 18:35:13 +0530

    DDAS WS

--- DDAS.API/WS/ddaswebservice.asmx.cs
+++ DDAS.API/WS/ddaswebservice.asmx.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using DDAS.Data.Mongo;
+using DDAS.Models.Entities.Domain;
+using DDAS.Services.Search;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using WebScraping.Selenium.SearchEngine;
+using static DDAS.Models.ViewModels.RequestPayloadforDDAS;
 
 namespace DDAS.API.WS
 {
@@ -21,6 +26,41 @@ namespace DDAS.API.WS
         public string HelloWorld()
         {
             return "Hello World";
+        }
+
+        [WebMethod]
+        public ComplianceForm iSprintToDDAS(ddRequest DR)
+        {
+            var ConnectionString =
+               System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            var DBName =
+                System.Configuration.ConfigurationManager.AppSettings["DBName"];
+
+            var _uow = new UnitOfWork(ConnectionString, DBName);
+            var _config = new Config();
+            var _SearchEngine = new SearchEngine(_uow, _config);
+            ComplianceFormService c = new ComplianceFormService(_uow,_SearchEngine,_config);
+            var obj = c.ImportIsprintData(DR);
+            return obj;
+        }
+
+        [WebMethod]
+        public ComplianceForm iSprintToDDASVerify(string Recid)
+        {
+
+            var ConnectionString =
+              System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            var DBName =
+                System.Configuration.ConfigurationManager.AppSettings["DBName"];
+
+            var _uow = new UnitOfWork(ConnectionString, DBName);
+            var _config = new Config();
+            var _SearchEngine = new SearchEngine(_uow, _config);
+            ComplianceFormService c = new ComplianceFormService(_uow, _SearchEngine, _config);
+            var obj = c.GetComplianceForm(Guid.Parse(Recid));
+            return obj;
         }
     }
 }
