c7c3b7c6b8377eeece7423d5bd2810fb45dc9c98
Patrick Taur <Patrick Taur>
2018-01-13 17:20:40 +0530

    in progress

--- DDAS.WebSite/app/search/shared/services/comp-form-logic.service.ts
+++ DDAS.WebSite/app/search/shared/services/comp-form-logic.service.ts
@@ -2,69 +2,70 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../../../auth/auth.service';
 import {
-    Finding,
+    Finding,CurrentReviewStatusViewModel
 } from '../../search.classes';
 
 @Injectable()
 export class CompFormLogicService {
+    public currentReviewStatus: CurrentReviewStatusViewModel;
     constructor(
-        private authService: AuthService
+        private authService: AuthService, ReviewStatusEnum
     ) { }
 
-    // selectFindingComponentToDisplay(selectedFinding: Finding, componentName: string) {
-    //     switch (componentName) {
-    //         case "findingEdit":
-    //             if (this.currentReviewStatus != undefined &&
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
-    //                 (this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.ReviewInProgress ||
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.SearchCompleted))
-    //                 return true;
-    //             else
-    //                 return false;
-    //         case "qcVerifierComments":
-    //             if (this.currentReviewStatus != undefined &&
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCInProgress &&
-    //                 selectedFinding.ReviewId != this.currentReviewStatus.QCVerifierRecId)
-    //                 return true;
-    //             else
-    //                 return false;
-    //         case "qcVerifierFinding":
-    //             if (this.currentReviewStatus != undefined &&
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCInProgress &&
-    //                 selectedFinding.ReviewId == this.currentReviewStatus.QCVerifierRecId)
-    //                 return true;
-    //             else
-    //                 return false;
-    //         case "responseToQCVerifierComments":
-    //             if (this.currentReviewStatus != undefined &&
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCCorrectionInProgress &&
-    //                 selectedFinding.ReviewId == this.currentReviewStatus.ReviewerRecId)
-    //                 return true;
-    //             else
-    //                 return false;
-    //         case "responseToQCVerifierFinding":
-    //             if (this.currentReviewStatus != undefined &&
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCCorrectionInProgress &&
-    //                 selectedFinding.ReviewId != this.currentReviewStatus.ReviewerRecId)
-    //                 return true;
-    //             else
-    //                 return false;
-    //         case "findingView":
-    //             // return true;
-    //             if (this.currentReviewStatus != undefined &&
-    //                 (this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.Completed ||
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCFailed ||
-    //                 this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCRequested ||
-    //                 this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() != this.authService.userName.toLowerCase()))
-    //                 return true;
-    //             else
-    //                 return false;
-    //         default: return false;
-    //     }
-    // }
+    selectFindingComponentToDisplay(selectedFinding: Finding, componentName: string) {
+        switch (componentName) {
+            case "findingEdit":
+                if (this.currentReviewStatus != undefined &&
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
+                    (this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.ReviewInProgress ||
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.SearchCompleted))
+                    return true;
+                else
+                    return false;
+            case "qcVerifierComments":
+                if (this.currentReviewStatus != undefined &&
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCInProgress &&
+                    selectedFinding.ReviewId != this.currentReviewStatus.QCVerifierRecId)
+                    return true;
+                else
+                    return false;
+            case "qcVerifierFinding":
+                if (this.currentReviewStatus != undefined &&
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCInProgress &&
+                    selectedFinding.ReviewId == this.currentReviewStatus.QCVerifierRecId)
+                    return true;
+                else
+                    return false;
+            case "responseToQCVerifierComments":
+                if (this.currentReviewStatus != undefined &&
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCCorrectionInProgress &&
+                    selectedFinding.ReviewId == this.currentReviewStatus.ReviewerRecId)
+                    return true;
+                else
+                    return false;
+            case "responseToQCVerifierFinding":
+                if (this.currentReviewStatus != undefined &&
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() == this.authService.userName.toLowerCase() &&
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCCorrectionInProgress &&
+                    selectedFinding.ReviewId != this.currentReviewStatus.ReviewerRecId)
+                    return true;
+                else
+                    return false;
+            case "findingView":
+                // return true;
+                if (this.currentReviewStatus != undefined &&
+                    (this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.Completed ||
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCFailed ||
+                    this.currentReviewStatus.CurrentReview.Status == ReviewStatusEnum.QCRequested ||
+                    this.currentReviewStatus.CurrentReview.AssigendTo.toLowerCase() != this.authService.userName.toLowerCase()))
+                    return true;
+                else
+                    return false;
+            default: return false;
+        }
+    }
 
 }
