4564f39001e3c14c2e64f97930860f0deeb26d2d
Patrick Taur <Patrick Taur>
2018-04-26 11:04:04 +0530

    in progress

---
+++ DDAS.WebSite/app/shared/components/not-found.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit  } from '@angular/core';
+@Component({ 
+    template: `
+    <div class="well">
+        Not Found
+    </div>
+    `,
+    
+
+ })
+export class NotFoundComponent implements OnInit {
+
+     ngOnInit() {
+         
+
+     }
+
+    
+    
+}
