6a9513167b912e0cdb8615a078d9f70dc050c423
Patrick Taur <Patrick Taur>
2019-03-30 12:31:19 +0530

    unused code and files removed

--- DDAS.API/Startup.cs
+++ DDAS.API/Startup.cs
@@ -20,7 +20,6 @@ namespace DDAS.API
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             var config = new HttpConfiguration();
             var logFile = HttpRuntime.AppDomainAppPath + "PerformanceMonitor.csv";
-            //config.MessageHandlers.Add(new CustomLogHandler(logFile));
             config.Filters.Add(new ExecutionTimeFilterAttribute(logFile));
             //config.Routes.MapHttpRoute("DefaultAPI",
             //    "api/{controller}/{id}",
