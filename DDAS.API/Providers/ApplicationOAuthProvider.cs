a66b4c3d35f0507d14e76e99062ceddb0dac4fab
Patrick Taur <patrick.taur@claritytechnologies.com>
2020-07-13 12:16:27 +0530

    version number update

--- DDAS.API/Providers/ApplicationOAuthProvider.cs
+++ DDAS.API/Providers/ApplicationOAuthProvider.cs
@@ -23,7 +23,7 @@ namespace DDAS.API.Providers
         private readonly Func<UserManager<IdentityUser, Guid>> _userManagerFactory;
 
         private IUserService _UserService;
-        private string _ClientVer = "1.1.5";
+        private string _ClientVer = "1.1.6";
 
         //public ApplicationOAuthProvider(string publicClientId, Func<UserManager<IdentityUser, Guid>> userManagerFactory)
         //{d
